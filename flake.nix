85269ea89be66fe754b292f5d447832bbe0b5bc7
neko-soft <rodrigo.pereira.riquelme@gmail.com>
2025-03-21 21:49:10 -0300

    Actualización de Archivos

--- flake.nix
+++ flake.nix
@@ -37,23 +37,23 @@
 
 	          conda
             # Instala python 3.11
-            python311
-            (python311.withPackages (ps: with ps; [
+            #python311
+            #(python311.withPackages (ps: with ps; [
               # Instala paquetes de python.
               # En teoría se puede usar pip install, pero lo ideal es usar sólo el flake
-              pandas numpy matplotlib seaborn scikit-learn openpyxl
-              xlrd jupyterlab yfinance pytz tensorflow keras
-            ]))
+            #  pandas numpy matplotlib seaborn scikit-learn openpyxl
+            #  xlrd jupyterlab yfinance pytz tensorflow keras
+            #]))
           ];
         shellHook = ''
           echo "Entorno de desarrollo activado para IPSA Prediction :D"
 
 
-          export PATH=${pkgs.cudatoolkit}/bin:$PATH
-          export CUDA_HOME=${pkgs.cudatoolkit}
-          export LD_LIBRARY_PATH=${pkgs.cudatoolkit}/lib:$LD_LIBRARY_PATH
-          export LD_LIBRARY_PATH=${pkgs.cudaPackages.cudnn}/lib:$LD_LIBRARY_PATH
-          export TF_CUDA_COMPUTE_CAPABILITIES="8.6" # Ajusta según tu GPU
+          #export PATH=${pkgs.cudatoolkit}/bin:$PATH
+          #export CUDA_HOME=${pkgs.cudatoolkit}
+          #export LD_LIBRARY_PATH=${pkgs.cudatoolkit}/lib:$LD_LIBRARY_PATH
+          #export LD_LIBRARY_PATH=${pkgs.cudaPackages.cudnn}/lib:$LD_LIBRARY_PATH
+          #export TF_CUDA_COMPUTE_CAPABILITIES="8.6" # Ajusta según tu GPU
 
         '';
 
